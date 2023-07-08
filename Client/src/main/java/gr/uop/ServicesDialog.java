e0e84ad443ae839dc68c4bd12ddb56ccc7fd0a29
Vasilis Koulouris <basileskouloures@gmail.com>
2023-07-08 09:20:22 +0300

    reworked service selection UI

--- Client/src/main/java/gr/uop/ServicesDialog.java
+++ Client/src/main/java/gr/uop/ServicesDialog.java
@@ -6,78 +6,80 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.Map.Entry;
 import java.util.Iterator;
-
+import javafx.scene.control.Separator;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ServicesDialog extends Dialog<Service[]>  implements EventHandler<ActionEvent>{
-    private Map<Service, CheckBox> list;
+    private Map<Service, CheckBox[]> list;
     private String[] names = {"Πλύσιμο εξωτερικό", "Πλύσιμο εσωτερικό", "Πλύσιμο εξωτ.+εσωτ.", "Πλύσιμο εξωτ. σπέσιαλ", "Πλύσιμο εσωτ. σπέσιαλ",
     "Πλύσιμο εξωτ.+εσωτ. σπέσιαλ", "Βιολογικός καθαρισμός εσωτ.", "Κέρωμα-Γυάλισμα", "Καθαρισμός κινητήρα", "Πλύσιμο σασί"
-}; 
+    };
+    private int totalCost = 0;
+    private Label totCost = new Label("Συνολικό ποσό: "+totalCost);
 
     public ServicesDialog(Stage stage){
         list = new TreeMap<>();
         
         for(int i = 0; i < names.length; i++){
-            CheckBox b = new CheckBox();
+            CheckBox b[] = new CheckBox[3];
             Service s = new Service(i, names[i]);
             list.put(s, b);
-            b.selectedProperty().addListener((e)->{
-                Service sb = new Service(s.getName());
-                Set<Map.Entry<Service, CheckBox>> entries = list.entrySet();
-                for(Map.Entry<Service, CheckBox> l: entries){
-                    if(l.getKey().equals(sb)){
-                        System.out.println(l.getKey().getName());
-                    }
-                }
-            });
         }
         setServicesCosts();
         
-        GridPane mainPane = new GridPane();
-        mainPane.setHgap(10);
-        mainPane.setVgap(5);
-        mainPane.setStyle("-fx-font-size: 20px;");
+        GridPane servicesPane = new GridPane();
+        servicesPane.setHgap(10);
+        servicesPane.setVgap(5);
+        servicesPane.setStyle("-fx-font-size: 20px;");
 
-        Set<Map.Entry<Service, CheckBox>> entries = list.entrySet();
-        Iterator<Entry<Service, CheckBox>> it = entries.iterator();
-        Entry<Service, CheckBox> e;
-        int c;
+        Set<Map.Entry<Service, CheckBox[]>> entries = list.entrySet();
+        Iterator<Entry<Service, CheckBox[]>> it = entries.iterator();
+        Entry<Service, CheckBox[]> e;
 
-        mainPane.add(new Label("Υπηρεσία"), 1, 0);
-        mainPane.add(new Label("Αυτοκίνητο"), 2, 0);
-        mainPane.add(new Label("Τζίπ"), 3, 0);
-        mainPane.add(new Label("Μοτοσυκλέτα"), 4, 0);
+        servicesPane.add(new Label("Υπηρεσία"), 0, 0);
+        servicesPane.add(new Label("Αυτοκίνητο"), 1, 0);
+        servicesPane.add(new Label("Τζίπ"), 2, 0);
+        servicesPane.add(new Label("Μοτοσυκλέτα"), 3, 0);
         for(int i=1; i < names.length; i++){
             if(it.hasNext()){
                 e = it.next();
             }else{break;}
-            mainPane.add(e.getValue(), 0, i);
-            mainPane.add(new Label(e.getKey().getName()), 1, i);
-            Label a, j, m;
-            c = e.getKey().getCarCost();
-            a = new Label(Integer.toString(c));
-            mainPane.add(a, 2, i);
-            c = e.getKey().getJeepCost();
-            j = new Label(Integer.toString(c));
-            mainPane.add(j, 3, i);
-            c = e.getKey().getMotorcycleCost();
-            if(c == -1){
-                m = new Label("-");
-            }else{m  = new Label(Integer.toString(c));}
-            mainPane.add(m, 4, i);
-            GridPane.setHalignment(a, HPos.CENTER);
-            GridPane.setHalignment(j, HPos.CENTER);    
-            GridPane.setHalignment(m, HPos.CENTER);
+            servicesPane.add(new Label(e.getKey().getName()), 0, i);
+            int k = 1;
+            CheckBox[] table = e.getValue();
+            for(int j = 0; j < table.length; j++){
+                table[j] = new CheckBox();
+                servicesPane.add(table[j], k, i);
+                k+=1;
+                GridPane.setHalignment(table[j], HPos.CENTER);
+            }
+            if(table.length == 2){
+                Label l = new Label("-");
+                servicesPane.add(l, 3, i);
+                GridPane.setHalignment(l, HPos.CENTER);
+            }
         }
+        
+        VBox status = new VBox();
+        status.getChildren().add(new Separator());
+        status.getChildren().add(totCost);
+        status.setPadding(new Insets(5, 0, 0, 0));
+        status.setStyle("-fx-font-size: 20px;");
+
+        BorderPane mainPane = new BorderPane();
+        mainPane.setCenter(servicesPane);
+        mainPane.setBottom(status);
 
         getDialogPane().setContent(mainPane);
         getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
@@ -86,29 +88,34 @@ public class ServicesDialog extends Dialog<Service[]>  implements EventHandler<A
     }
 
     private void setServicesCosts() {
-        Set<Map.Entry<Service,CheckBox>> entries = list.entrySet();
-        for(Map.Entry<Service,CheckBox> e: entries){
+        Set<Map.Entry<Service,CheckBox[]>> entries = list.entrySet();
+        for(Map.Entry<Service,CheckBox[]> e: entries){
             switch(e.getKey().getName()){
                 case "Πλύσιμο εξωτερικό":
                     e.getKey().setCost(7, 8, 6);
                     break;
                 case "Πλύσιμο εσωτερικό":
                     e.getKey().setCost(6, 7);
+                    e.setValue(new CheckBox[2]);
                     break;
                 case "Πλύσιμο εξωτ.+εσωτ.":
                     e.getKey().setCost(12, 14);
+                    e.setValue(new CheckBox[2]);
                     break;
                 case "Πλύσιμο εξωτ. σπέσιαλ":
                     e.getKey().setCost(9, 10, 8);
                     break;
                 case "Πλύσιμο εσωτ. σπέσιαλ":
                     e.getKey().setCost(8, 9);
+                    e.setValue(new CheckBox[2]);
                     break;
                  case "Πλύσιμο εξωτ.+εσωτ. σπέσιαλ":
                     e.getKey().setCost(15, 17);
+                    e.setValue(new CheckBox[2]);
                     break;
                 case "Βιολογικός καθαρισμός εσωτ.":
                     e.getKey().setCost(80, 80);
+                    e.setValue(new CheckBox[2]);
                     break;
                 case "Κέρωμα-Γυάλισμα":
                     e.getKey().setCost(80, 90, 40);
@@ -118,6 +125,7 @@ public class ServicesDialog extends Dialog<Service[]>  implements EventHandler<A
                     break;
                 case "Πλύσιμο σασί":
                     e.getKey().setCost(3, 3);
+                    e.setValue(new CheckBox[2]);
                     break;
             }
         }
